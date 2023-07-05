f6b8b5ece1ecfffd1644678a8b763bf7f35114e9
ck412.park <pigun412@gmail.com>
2023-07-05 15:09:36 +0900

    TDD Cycle 1

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,12 @@
 #include "pch.h"
+#include "../230705_CRA_PrimeFactor/PrimeFactor.cpp"
+#include <vector>
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+TEST(PrimeFactors, Of1) {
+
+	PrimeFactor prime_factor;
+	vector<int> expected = {};
+	EXPECT_EQ(expected, prime_factor.of(1));
+
 }
