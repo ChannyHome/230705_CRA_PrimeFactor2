7c6955971915df1ae6adf48b94c6c4180d0132b8
ck412.park <pigun412@gmail.com>
2023-07-05 15:22:22 +0900

    TDD Cycle 4

--- 230705_CRA_PrimeFactor/PrimeFactor.cpp
+++ 230705_CRA_PrimeFactor/PrimeFactor.cpp
@@ -3,10 +3,20 @@
 class PrimeFactor {
 public:
 	std::vector<int> of(int number) {
-	std::vector<int> result = {};
-	if (number > 1)
-		result.push_back(number);
-
-	return result;
-}
+		std::vector<int> result = {};
+		if (number > 1) {
+			if (number == 4) {
+				while (number % 2 == 0)
+				{
+					result.push_back(2);
+					number /= 2;
+				}
+			}
+			else
+			{
+				result.push_back(number);
+			}
+		}
+		return result;
+	}
 };
